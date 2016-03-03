7bb40f45352667c52d23d4336290f9cc8d6cc216
Claudio Brandolino <cbrandolino@gmail.com>
2016-03-03 12:51:01 +0100

    Added :current flag to Item

--- test/models/vasari/item_test.rb
+++ test/models/vasari/item_test.rb
@@ -14,31 +14,31 @@ module Vasari
       item.date = Date.today
       assert item.save, "Saved Item with title and date"
     end
-    class DateHandlingTest < ActiveSupport::TestCase
-      test "only date_start" do 
-        item = Item.new
-        item.title = "My title"
-        item.date_start = Date.yesterday
-        item.save
-        assert_equal item.date, Date.today, "Date set to today"
-        assert item.current, "Current set to true"
-      end
-      test "only date_end" do 
-        item = Item.new
-        item.title = "My title"
-        item.date_end = Date.yesterday
-        item.save
-        assert_equal item.date, Date.yesterday, "Date set to date_end"
-        assert_not item.current, "Current set to false"
-      end
-      test "date set directly" do 
-        item = Item.new
-        item.title = "My title"
-        item.date = Date.yesterday
-        item.save
-        assert_equal item.date, Date.yesterday, "Date unchanged"
-        assert_not item.current, "Current set to false"
-      end
+  end
+  class ItemTest::DateHandlingTest < ActiveSupport::TestCase
+    test "only date_start" do 
+      item = Item.new
+      item.title = "My title"
+      item.date_start = Date.yesterday
+      item.save
+      assert_equal item.date, Date.today, "Date set to today"
+      assert item.current, "Current set to true"
+    end
+    test "only date_end" do 
+      item = Item.new
+      item.title = "My title"
+      item.date_end = Date.yesterday
+      item.save
+      assert_equal item.date, Date.yesterday, "Date set to date_end"
+      assert_not item.current, "Current set to false"
+    end
+    test "date set directly" do 
+      item = Item.new
+      item.title = "My title"
+      item.date = Date.yesterday
+      item.save
+      assert_equal item.date, Date.yesterday, "Date unchanged"
+      assert_not item.current, "Current set to false"
     end
   end
 end

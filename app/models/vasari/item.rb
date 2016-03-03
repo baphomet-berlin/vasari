91905ca6d6525dd727c703676d7d285ff946c0f4
Claudio Brandolino <cbrandolino@gmail.com>
2016-03-03 12:49:13 +0100

    Added item date tests

--- app/models/vasari/item.rb
+++ app/models/vasari/item.rb
@@ -2,14 +2,17 @@ module Vasari
   class Item < ActiveRecord::Base
     validates :title, presence: true
     validates :date, presence: true
-    before_save :handle_date
+    before_validation :handle_date
 
     def handle_date()
       if (self.date)
+        self.current |= false;
         return
-      elsif (self.end_date)
-        self.date = self.end_date
-      elsif (self.start_date)
+      elsif (self.date_end)
+        self.current |= false;
+        self.date = self.date_end
+      elsif (self.date_start)
+        self.current |= true;
         self.date = Date.today
       end
     end
--- test/models/vasari/item_test.rb
+++ test/models/vasari/item_test.rb
@@ -2,21 +2,43 @@ require 'test_helper'
 
 module Vasari
   class ItemTest < ActiveSupport::TestCase
-    test "does not save item without title" do
+    test "Basic validation" do
       item = Item.new
       item.date = Date.today
-      assert_not item.save, "Saved Item without title"
-    end
-    test "does not save item without date" do
+      assert_not item.save, "Did not save item without title"
       item = Item.new
       item.title = "My title"
-      assert_not item.save, "Saved Item without date"
-    end
-    test "saves item with title and date" do
+      assert_not item.save, "Did not save Item without date"
       item = Item.new
       item.title = "My title"
       item.date = Date.today
-      assert item.save
+      assert item.save, "Saved Item with title and date"
+    end
+    class DateHandlingTest < ActiveSupport::TestCase
+      test "only date_start" do 
+        item = Item.new
+        item.title = "My title"
+        item.date_start = Date.yesterday
+        item.save
+        assert_equal item.date, Date.today, "Date set to today"
+        assert item.current, "Current set to true"
+      end
+      test "only date_end" do 
+        item = Item.new
+        item.title = "My title"
+        item.date_end = Date.yesterday
+        item.save
+        assert_equal item.date, Date.yesterday, "Date set to date_end"
+        assert_not item.current, "Current set to false"
+      end
+      test "date set directly" do 
+        item = Item.new
+        item.title = "My title"
+        item.date = Date.yesterday
+        item.save
+        assert_equal item.date, Date.yesterday, "Date unchanged"
+        assert_not item.current, "Current set to false"
+      end
     end
   end
 end

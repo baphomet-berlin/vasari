11c124c52ad57e57b3c932891f7e21eac7bacd81
Claudio Brandolino <cbrandolino@gmail.com>
2016-03-03 12:53:02 +0100

    Refactored Item tests

--- test/models/vasari/item_test.rb
+++ test/models/vasari/item_test.rb
@@ -16,29 +16,27 @@ module Vasari
     end
   end
   class ItemTest::DateHandlingTest < ActiveSupport::TestCase
+    setup do
+      @item = Item.new
+      @item.title = "My title"  
+    end
     test "only date_start" do 
-      item = Item.new
-      item.title = "My title"
-      item.date_start = Date.yesterday
-      item.save
-      assert_equal item.date, Date.today, "Date set to today"
-      assert item.current, "Current set to true"
+      @item.date_start = Date.yesterday
+      @item.save
+      assert_equal @item.date, Date.today, "Date set to today"
+      assert @item.current, "Current set to true"
     end
     test "only date_end" do 
-      item = Item.new
-      item.title = "My title"
-      item.date_end = Date.yesterday
-      item.save
-      assert_equal item.date, Date.yesterday, "Date set to date_end"
-      assert_not item.current, "Current set to false"
+      @item.date_end = Date.yesterday
+      @item.save
+      assert_equal @item.date, Date.yesterday, "Date set to date_end"
+      assert_not @item.current, "Current set to false"
     end
-    test "date set directly" do 
-      item = Item.new
-      item.title = "My title"
-      item.date = Date.yesterday
-      item.save
-      assert_equal item.date, Date.yesterday, "Date unchanged"
-      assert_not item.current, "Current set to false"
+    test "date set directly" do
+      @item.date = Date.yesterday
+      @item.save
+      assert_equal @item.date, Date.yesterday, "Date unchanged"
+      assert_not @item.current, "Current set to false"
     end
   end
 end

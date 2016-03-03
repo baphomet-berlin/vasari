81028daba167c25012d9392e89d897097e6f7f59
Claudio Brandolino <cbrandolino@gmail.com>
2016-03-03 08:29:03 +0100

    Added basic item tests

--- app/models/vasari/item.rb
+++ app/models/vasari/item.rb
@@ -1,4 +1,17 @@
 module Vasari
   class Item < ActiveRecord::Base
+    validates :title, presence: true
+    validates :date, presence: true
+    before_save :handle_date
+
+    def handle_date()
+      if (self.date)
+        return
+      elsif (self.end_date)
+        self.date = self.end_date
+      elsif (self.start_date)
+        self.date = Date.today
+      end
+    end
   end
 end
--- test/models/vasari/item_test.rb
+++ test/models/vasari/item_test.rb
@@ -2,8 +2,21 @@ require 'test_helper'
 
 module Vasari
   class ItemTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "does not save item without title" do
+      item = Item.new
+      item.date = Date.today
+      assert_not item.save, "Saved Item without title"
+    end
+    test "does not save item without date" do
+      item = Item.new
+      item.title = "My title"
+      assert_not item.save, "Saved Item without date"
+    end
+    test "saves item with title and date" do
+      item = Item.new
+      item.title = "My title"
+      item.date = Date.today
+      assert item.save
+    end
   end
 end

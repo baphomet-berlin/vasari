3570feb9dacdf083f815c278c23b00c6beab48fb
Claudio Brandolino <cbrandolino@gmail.com>
2016-03-10 00:40:10 +0100

    Added item collaborator uniqueness tests

--- app/models/vasari/item_collaborator.rb
+++ app/models/vasari/item_collaborator.rb
@@ -3,5 +3,9 @@ module Vasari
     belongs_to :item
     belongs_to :collaborator
     belongs_to :role
+    validates :item_id, presence: :true
+    validates :collaborator_id, presence: :true
+    validates :role_id, presence: true
+    validates :item_id, uniqueness: { scope: [:role_id, :collaborator_id] }
   end
 end

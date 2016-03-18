5e3fee28e3dd0f71b3e168e9b0c13d075b9e796a
Claudio Brandolino <cbrandolino@gmail.com>
2016-03-18 03:30:01 +0100

    Removed unused and erroneous fixture

--- app/models/vasari/item_collaborator.rb
+++ app/models/vasari/item_collaborator.rb
@@ -5,7 +5,7 @@ module Vasari
     belongs_to :role
     validates :item_id, presence: :true
     validates :collaborator_id, presence: :true
-    validates :role_id, presence: true
+    validates :role, presence: true
     validates :item_id, uniqueness: { scope: [:role_id, :collaborator_id] }
   end
 end

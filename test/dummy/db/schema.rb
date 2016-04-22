b38ac5c6d55ec1b7b75e369686a77a6d6a79d25c
Claudio Brandolino <cbrandolino@gmail.com>
2016-04-22 02:49:51 +0200

    Added slug field to vasari project

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421150326) do
+ActiveRecord::Schema.define(version: 20160422004914) do
 
   create_table "vasari_collaborators", force: :cascade do |t|
     t.string   "name"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20160421150326) do
     t.text     "content"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "slug"
   end
 
   create_table "vasari_roles", force: :cascade do |t|
